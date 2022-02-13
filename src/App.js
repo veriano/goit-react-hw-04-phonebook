1b49785302b7068079fba83a592ff57881ab8766
veriano <86019033+veriano@users.noreply.github.com>
2022-02-13 14:36:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,76 +1,76 @@
 import { useState, useEffect } from 'react';
-// import useLocalStorage from './hooks/useLocalStorage';
 import { v4 as uuidv4 } from 'uuid';
 import s from './App.module.css';
 import ContactForm from './Components/ContactForm';
 import ContactList from './Components/ContactsList';
 import Filter from './Components/Filter';
 
-
-
 function App() {
-  
   const [contacts, setContacts] = useState(() => {
-    return JSON.parse(localStorage.getItem("contacts")) ?? [];
+    return JSON.parse(window.localStorage.getItem('contacts')) ?? [];
   });
+
   const [filter, setFilter] = useState('');
 
-  useEffect(()=>{
-    window.localStorage.setItem("contacts", JSON.stringify(contacts));
-  },[contacts])
-
+  useEffect(() => {
+    window.localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const changeFilter = e => {
     setFilter(e.currentTarget.value);
-  }
+  };
 
-  const addContact = (data) => {
+  const addContact = ({ name, number }) => {
+    const contactName = contacts.find(
+      contact => name.toLowerCase() === contact.name.toLowerCase(),
+    );
+
+    if (contactName) {
+      alert(`${name} already in contacts.`);
+      return;
+    }
+
     const contact = {
       id: uuidv4(),
-      name: data.name,
-      number: data.number,
-    }
-    console.log(contact);
-    if(contacts !== null) {
-      const contactName = contacts.find(contact => contact.name.toLowerCase() === data.name.toLowerCase());
-    
-      if(contactName) {
-        alert(`${data.name} already in contacts.`);
-        return; 
-      }
-      setContacts(prevContacts => [...contact,...prevContacts]);
-    }
-  }
+      name,
+      number,
+    };
 
-  const deleteContact = (contactId) => {
-    setContacts(contacts.filter(contact => contact.id !== contactId));
-  }
+    setContacts([...contacts, contact]);
+  };
+
+  const deleteContact = contactId => {
+    setContacts(contacts => contacts.filter(({ id }) => id !== contactId));
+  };
 
   const getVisibleContacts = () => {
-    const normolizedFilter = filter.toLowerCase();
+    const normalizedFilter = filter.toLowerCase();
 
-    if(contacts !== null) {
-    return setContacts(contacts.filter(contact => contact.name.toLowerCase().includes(normolizedFilter)));
-    }
-  }
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
 
-  const visibleContacts = getVisibleContacts();
-   
-    return (
-      <>
-        <h1 className={ s.Title }><b>Phonebook</b></h1>
+  return (
+    <>
+      <h1 className={s.Title}>
+        <b>Phonebook</b>
+      </h1>
 
-        <ContactForm onSubmitHandler={ addContact } />
-      
-        <h2 className={ s.Title }><b>Contacts</b></h2>
+      <ContactForm onSubmitHandler={addContact} />
 
-        <Filter value={ filter } onChange={ changeFilter }/>
+      <h2 className={s.Title}>
+        <b>Contacts</b>
+      </h2>
 
-        <ContactList
-          contacts={ visibleContacts }
-          onDeleteContact={ deleteContact }  />
-      </>
-    )
-  }
+      <Filter value={filter} onChange={changeFilter} />
+
+      <ContactList
+        contacts={getVisibleContacts()}
+        onDeleteContact={deleteContact}
+      />
+    </>
+  );
+}
 
 export default App;
